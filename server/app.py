2266aa8e89538fa15f8f98d13d6cabcbf8572cb0
Mark <markkamamia@gmail.com>
2024-07-03 14:40:30 +0300

    lab

--- server/app.py
+++ server/app.py
@@ -6,33 +6,67 @@ from flask_migrate import Migrate
 from models import db, Bakery, BakedGood
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return '<h1>Bakery GET API</h1>'
+    return "<h1>Bakery GET API</h1>"
 
-@app.route('/bakeries')
+
+@app.route("/bakeries")
 def bakeries():
-    return ''
 
-@app.route('/bakeries/<int:id>')
+    bakery_s = Bakery.query.all()
+
+    bakeryjson = [bakery.to_dict() for bakery in bakery_s]
+
+    return make_response(jsonify(bakeryjson), 200)
+
+
+@app.route("/bakeries/<int:id>")
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter(Bakery.id == id).first()
 
-@app.route('/baked_goods/by_price')
+    if bakery:
+        body = bakery.to_dict()
+        status = 200
+
+    else:
+        body = {"message": f"bakery {id} not found"}
+        status = 404
+
+    response = make_response(body, status, {"Content-Type": "application/json"})
+    return response
+
+
+@app.route("/baked_goods/by_price")
 def baked_goods_by_price():
-    return ''
 
-@app.route('/baked_goods/most_expensive')
+    baked_goods = [
+        good.to_dict()
+        for good in BakedGood.query.order_by(BakedGood.price.desc()).all()
+    ]
+
+    response = make_response(baked_goods, 200, {"Content-Type": "application/json"})
+
+    return response
+
+
+@app.route("/baked_goods/most_expensive")
 def most_expensive_baked_good():
-    return ''
 
-if __name__ == '__main__':
+    expensive_good = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    response = make_response(expensive_good.to_dict(),200)
+    return response
+
+
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
